f9f103759e347df15e6d43f34d406489b5af119d
protechassist36-admin <strangesteven001@gmail.com>
2026-07-09 22:59:20 +0000

    style: replace transparent favicon with solid background jpeg to fix dark tab visibility

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
-        <link rel="apple-touch-icon" href="/images/logo2.png" />
+        <link rel="apple-touch-icon" href="/images/logo2.jpeg" />
       </head>
       <body className="min-h-full flex flex-col font-sans">
         <ThemeProvider
